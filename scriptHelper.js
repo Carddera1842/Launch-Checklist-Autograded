23b849bc5a3d1b45bf244d8d2b26ac530f358ef2
Carddera1842 <carddera1842@gmail.com>
2024-08-15 20:12:43 -0500

    "boop"

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,9 +1,6 @@
-// Write your helper functions here!
-
 require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
     let missionTarget = document.getElementById("missionTarget");
     missionTarget.innerHTML = `
                  <h2>Mission Destination</h2>
@@ -36,11 +33,9 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     let launchStatus = document.getElementById("launchStatus")
     
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
-        alert("All fields are required!")
-        event.preventDefault();
+        window.alert("All fields are required!")
     } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number" || validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
-        alert("Fuel Level/Cargo Level is not a number OR Pilot/Copilot is a number!");
-        event.preventDefault();
+        window.alert("Fuel Level/Cargo Level is not a number OR Pilot/Copilot is a number!");
     }
 
     pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
